69a0d29f3c6c798498fb5f12db474cc7c4466f6a
Mark Njung'e <mark.kamau@outlook.com>
2018-06-17 19:08:39 +0300

    Add logout button

--- app/src/main/java/com/marknkamau/globalgym/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/marknkamau/globalgym/ui/profile/ProfileFragment.kt
@@ -1,17 +1,31 @@
 package com.marknkamau.globalgym.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.marknkamau.globalgym.App
 
 import com.marknkamau.globalgym.R
+import com.marknkamau.globalgym.ui.login.LoginActivity
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button.setOnClickListener {
+            App.authService.signOut()
+            startActivity(Intent(activity, LoginActivity::class.java))
+            activity!!.finish()
+        }
     }
 
 }

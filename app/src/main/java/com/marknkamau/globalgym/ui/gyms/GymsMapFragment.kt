ad0a76e0d05b4a6990dc60bfa1d8a1617b106686
Mark Njung'e <mark.kamau@outlook.com>
2018-06-19 21:50:54 +0300

    Add Google Map

--- app/src/main/java/com/marknkamau/globalgym/ui/gyms/GymsMapFragment.kt
+++ app/src/main/java/com/marknkamau/globalgym/ui/gyms/GymsMapFragment.kt
@@ -5,13 +5,29 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 import com.marknkamau.globalgym.R
 
-class GymsMapFragment : Fragment() {
+class GymsMapFragment : Fragment(), OnMapReadyCallback {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_gyms_map, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fragment:SupportMapFragment = childFragmentManager.findFragmentById(R.id.mapGyms) as SupportMapFragment
+        fragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
 }

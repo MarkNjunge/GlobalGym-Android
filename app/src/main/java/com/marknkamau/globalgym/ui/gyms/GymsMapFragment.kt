42a1f280b0deaec4a82ac007c0bf4ac055cb8490
Mark Njung'e <mark.kamau@outlook.com>
2018-06-19 22:01:10 +0300

    Add permission request

--- app/src/main/java/com/marknkamau/globalgym/ui/gyms/GymsMapFragment.kt
+++ app/src/main/java/com/marknkamau/globalgym/ui/gyms/GymsMapFragment.kt
@@ -1,15 +1,18 @@
 package com.marknkamau.globalgym.ui.gyms
 
+import android.Manifest
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 import com.marknkamau.globalgym.R
+import com.tbruyelle.rxpermissions2.RxPermissions
 
 class GymsMapFragment : Fragment(), OnMapReadyCallback {
 
@@ -20,8 +23,17 @@ class GymsMapFragment : Fragment(), OnMapReadyCallback {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val fragment:SupportMapFragment = childFragmentManager.findFragmentById(R.id.mapGyms) as SupportMapFragment
-        fragment.getMapAsync(this)
+        val rxPermissions = RxPermissions(activity!!)
+        rxPermissions.request(Manifest.permission.ACCESS_FINE_LOCATION)
+                .subscribe { granted: Boolean ->
+                    if (granted) {
+                        val fragment: SupportMapFragment = childFragmentManager.findFragmentById(R.id.mapGyms) as SupportMapFragment
+                        fragment.getMapAsync(this)
+                    } else {
+                        Toast.makeText(activity, "Location permission is required", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
     }
 
     override fun onMapReady(googleMap: GoogleMap) {

05d9f110b9ec8169b37c1db71f741d175211cca0
MarkNjunge <mark.kamau@outlook.com>
2019-03-19 09:17:27 +0300

    Add missing files

---
+++ app/src/main/java/com/marknkamau/globalgym/ui/main/MainView.kt
@@ -0,0 +1,13 @@
+package com.marknkamau.globalgym.ui.main
+
+/**
+ * Created by MarkNjunge.
+ * mark.kamau@outlook.com
+ * https://github.com/MarkNjunge
+ */
+
+interface MainView{
+    fun onNotSignedIn()
+
+    fun onSignedIn()
+}
---
+++ app/src/main/java/com/marknkamau/globalgym/ui/settings/SettingsActivity.kt
@@ -0,0 +1,61 @@
+package com.marknkamau.globalgym.ui.settings
+
+import android.content.Intent
+import android.os.Bundle
+import com.marknkamau.globalgym.App
+import com.marknkamau.globalgym.BuildConfig
+import com.marknkamau.globalgym.R
+import com.marknkamau.globalgym.ui.base.BaseActivity
+import com.marknkamau.globalgym.ui.login.LoginActivity
+import com.marknkamau.globalgym.ui.main.MainActivity
+import com.marknkamau.globalgym.utils.LocaleManager
+import com.marknkamau.globalgym.utils.RxUtils
+import kotlinx.android.synthetic.main.activity_settings.*
+
+class SettingsActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        supportActionBar?.title = getString(R.string.settings)
+
+        val dialog = SelectLanguageDialog()
+        dialog.onSelected = { language ->
+            val code = LocaleManager.getCode(language)
+
+            App.settingsRepository.saveLanguageCode(code)
+
+            LocaleManager.updateResources(this, code)
+
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
+
+        val code = App.settingsRepository.getLanguageCode()
+        tvCurrentLanguage.text = LocaleManager.getLanguage(code)
+
+        layoutLanguage.setOnClickListener {
+            dialog.show(supportFragmentManager, "lang")
+        }
+
+        layoutLogout.setOnClickListener {
+            App.userRepository.deleteUserLocal()
+            App.userRepository.deletePreferredGymLocal()
+            App.sessionsRepository.deleteSessionsCache()
+                    .compose(RxUtils.applyCompletableSchedulers())
+            App.authService.signOut()
+
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        }
+
+        tvVersion.text = getString(R.string.version, BuildConfig.VERSION_NAME)
+
+    }
+}
